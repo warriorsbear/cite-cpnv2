f73992f747732b98e811d4f106a4ba44363f42c5
FloSho <florian.lepronguillout@gmail.com>
2024-12-27 15:52:48 +0100

    connexion impossible si compte non validé ok
    si le compte n'a pas était validé par un admin, un message d'erreur s'affiche lors de la connexion

---
+++ site_sae/app/Http/Controllers/Auth/CustomAuthenticatedSessionController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class CustomAuthenticatedSessionController extends Controller
+{
+    /**
+     * Display the login view.
+     */
+    public function create(): Response
+    {
+        return Inertia::render('Auth/Login', [
+            'canResetPassword' => Route::has('password.request'),
+            'status' => session('status'),
+        ]);
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            if ($user->statut == 0) {
+                Auth::logout();
+                throw ValidationException::withMessages([
+                    'email' => 'Votre compte n\'a pas encore était validé par les administrateurs.',
+                ]);
+            }
+
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Les informations ne correspondent pas à nos enregistrements.',
+        ]);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
